0f196a405e542366111220214db0b93efac2013f
Marco <marco@mirlo.org>
2023-06-25 21:24:55 +0200

    Añade Wifi a crate embsens.
    - Añade configuración de rustc (.cargo/config.toml) para incluir en el
      enlazado el código C compilado por el crate esp-wifi-sys.

---
+++ embsens/README.md
@@ -0,0 +1,8 @@
+# Ejemplo con Embassy (no-std), i2c y wifi
+
+Compilar con :
+
+    export SSID=myssid
+     export PASSWORD=mypassword
+    cargo build
+
--- embsens/src/main.rs
+++ embsens/src/main.rs
@@ -21,6 +21,27 @@ use hal::{
 use icm42670::{prelude::*, Address, Icm42670};
 use static_cell::StaticCell;
 
+
+use embassy_net::tcp::TcpSocket;
+use embassy_net::{Config, Ipv4Address, Stack, StackResources};
+use embedded_svc::wifi::{ClientConfiguration, Configuration, Wifi};
+use esp_wifi::wifi::{WifiController, WifiDevice, WifiEvent, WifiMode, WifiState};
+use esp_wifi::{initialize, EspWifiInitFor};
+use hal::Rng;
+
+const SSID: &str = env!("SSID");
+const PASSWORD: &str = env!("PASSWORD");
+
+macro_rules! singleton {
+    ($val:expr) => {{
+        type T = impl Sized;
+        static STATIC_CELL: StaticCell<T> = StaticCell::new();
+        let (x,) = STATIC_CELL.init(($val,));
+        x
+    }};
+}
+
+
 // I2C bus static reference for sharing bus between tasks
 static I2C_BUS: StaticCell<NoopMutex<RefCell<I2C<I2C0>>>> = StaticCell::new();
 
@@ -125,16 +146,47 @@ fn main() -> ! {
     wdt0.disable();
     wdt1.disable();
 
+
+    // let timer = hal::systimer::SystemTimer::new(peripherals.SYSTIMER).alarm0;
+    let timer = hal::systimer::SystemTimer::new(peripherals.SYSTIMER);
+
+    // wifi configuration
+    let init = initialize(
+        EspWifiInitFor::Wifi,
+        timer.alarm0,
+        Rng::new(peripherals.RNG),
+        system.radio_clock_control,
+        &clocks,
+    )
+    .unwrap();
+
+    let (wifi, _) = peripherals.RADIO.split();
+    let (wifi_interface, controller) = esp_wifi::wifi::new_with_mode(&init, wifi, WifiMode::Sta);
+
+    let config = Config::Dhcp(Default::default());
+
+    let seed = 1234; // very random, very secure seed
+
+    // Init network stack
+    let stack = &*singleton!(Stack::new(
+        wifi_interface,
+        config,
+        singleton!(StackResources::<3>::new()),
+        seed
+    ));
+   
+
     // Only option in ESP32-C3 for clocks
     // #[cfg(feature = "embassy-time-systick")]
-    embassy::init(
-        &clocks,
-        hal::systimer::SystemTimer::new(peripherals.SYSTIMER),
-    );
+    // embassy::init(
+    //     &clocks,
+    //     timer, // hal::systimer::SystemTimer::new(peripherals.SYSTIMER),
+        
+    // );
 
     // Not available in ESP32-C3
     //    #[cfg(feature = "embassy-time-timg0")]
-    //    embassy::init(&clocks, timer_group0.timer0);
+    embassy::init(&clocks, timer_group0.timer0);
 
     // i2c initialization. Pins GPIO10 SDA, GPIO8 CLK
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
@@ -162,8 +214,123 @@ fn main() -> ! {
         spawner.spawn(run2()).ok();
         spawner.spawn(run_i2c(i2c_dev1)).ok();
         spawner.spawn(run_htu(i2c_dev2)).ok();
+        
+        spawner.spawn(connection(controller)).ok();
+        spawner.spawn(net_task(&stack)).ok();
+        spawner.spawn(task(&stack)).ok();
     });
 
     // println!("Hello world!");
     // loop {}
+}
+
+
+
+
+// ----------------------- Conexión Wifi -------------------------
+
+#[embassy_executor::task]
+async fn connection(mut controller: WifiController<'static>) {
+    println!("start connection task");
+    println!("Device capabilities: {:?}", controller.get_capabilities());
+    loop {
+        match esp_wifi::wifi::get_wifi_state() {
+            WifiState::StaConnected => {
+                // wait until we're no longer connected
+                controller.wait_for_event(WifiEvent::StaDisconnected).await;
+                Timer::after(Duration::from_millis(5000)).await
+            }
+            _ => {}
+        }
+        if !matches!(controller.is_started(), Ok(true)) {
+            let client_config = Configuration::Client(ClientConfiguration {
+                ssid: SSID.into(),
+                password: PASSWORD.into(),
+                ..Default::default()
+            });
+            controller.set_configuration(&client_config).unwrap();
+            println!("Starting wifi");
+            controller.start().await.unwrap();
+            println!("Wifi started!");
+        }
+        println!("About to connect...");
+
+        match controller.connect().await {
+            Ok(_) => println!("Wifi connected!"),
+            Err(e) => {
+                println!("Failed to connect to wifi: {e:?}");
+                Timer::after(Duration::from_millis(5000)).await
+            }
+        }
+    }
+}
+
+
+#[embassy_executor::task]
+async fn net_task(stack: &'static Stack<WifiDevice<'static>>) {
+    stack.run().await
+}
+
+
+#[embassy_executor::task]
+async fn task(stack: &'static Stack<WifiDevice<'static>>) {
+    let mut rx_buffer = [0; 4096];
+    let mut tx_buffer = [0; 4096];
+
+    loop {
+        if stack.is_link_up() {
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
+
+    println!("Waiting to get IP address...");
+    loop {
+        if let Some(config) = stack.config() {
+            println!("Got IP: {}", config.address);
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
+
+    loop {
+        Timer::after(Duration::from_millis(1_000)).await;
+
+        let mut socket = TcpSocket::new(&stack, &mut rx_buffer, &mut tx_buffer);
+
+        socket.set_timeout(Some(embassy_net::SmolDuration::from_secs(10)));
+
+        let remote_endpoint = (Ipv4Address::new(142, 250, 185, 115), 80);
+        println!("connecting...");
+        let r = socket.connect(remote_endpoint).await;
+        if let Err(e) = r {
+            println!("connect error: {:?}", e);
+            continue;
+        }
+        println!("connected!");
+        let mut buf = [0; 1024];
+        loop {
+            use embedded_io::asynch::Write;
+            let r = socket
+                .write_all(b"GET / HTTP/1.0\r\nHost: www.mobile-j.de\r\n\r\n")
+                .await;
+            if let Err(e) = r {
+                println!("write error: {:?}", e);
+                break;
+            }
+            let n = match socket.read(&mut buf).await {
+                Ok(0) => {
+                    println!("read EOF");
+                    break;
+                }
+                Ok(n) => n,
+                Err(e) => {
+                    println!("read error: {:?}", e);
+                    break;
+                }
+            };
+            println!("{}", core::str::from_utf8(&buf[..n]).unwrap());
+        }
+        Timer::after(Duration::from_millis(3000)).await;
+    }
 }
